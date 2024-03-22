0db40b08109ccf07d3eea7124e763e95b6507ea2
ALEX DROBLAS <47006354@ort>
2024-03-22 11:26:54 -0300

    3er commit

--- src/controllers/events-controller.js
+++ src/controllers/events-controller.js
@@ -1,6 +1,22 @@
 import express from "express";
+import {
+    EventsService,
+    EventsService2,
+    EventsService3,
+  } from "../servicios/events-service.js";
+    const router = express.Router();
+    const evenService = new EventsService();
+    router.get("/",(request,response)=>{
+    const pageSize = req.query.pageSize;
+    const page = req.query.page;
 
+    const getAllEvents = EventsService.getAllEvents(pageSize, page)
+    return response.json(getAllEvents);
 
+})
 
-const router = express.Router();
+router.get("/", (request, repsonse)=>{
+
+})
+
 export default router;
--- src/servicios/events-service.js
+++ src/servicios/events-service.js
@@ -1,3 +1,17 @@
 class EventsService {
-    
+    getAllEvents(){
+        const pageSize = 10;
+        const requestedPage = 0;
+        //ir a base de datos...
+        query = 'select * from events limit $(pagesize) offset $(requestedPage)';
+        const eventsInDB = query.execute();
+
+        return{
+            collection: eventsInDB,
+            pageSize: pageSize,
+            page: requestedPage,
+            nextPage: requestedPage + 1,
+        }
+        
+    }
 }
