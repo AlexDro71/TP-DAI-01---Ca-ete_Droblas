dfbc96f08e1d8ce7f778c6b7241ca22bfb0244cf
ALEX DROBLAS <47006354@ort>
2024-06-24 15:59:52 -0300

    ASDIJASDJ

--- src/utils/auth-utils.js
+++ src/utils/auth-utils.js
@@ -7,7 +7,7 @@ import cors from 'cors'
             let token = vtoken;
             let payloadOriginal = null;
             try{
-                payloadOriginal =  jwt.verify(token, secretKey);
+                payloadOriginal = jwt.verify(token, secretKey);
             } catch (e){
                 console.error(e);
             }
@@ -17,6 +17,7 @@ export function authMiddleware(request, response, next){
     if(!request.headers.authorization){
         response.status(403).json({message: "Token no existe"})
     }else{
+        console.log(request.token)
         const desencriptado = desencriptarToken(request.token)
         if(!desencriptado){
             response.status(401).json({message: "No autenticado"})
